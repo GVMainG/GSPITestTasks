70a4f8cbb751ffc13182b2e0e50f6c1492cf256b
Georgeiy <GV.MainG@gmail.com>
2024-03-19 17:12:42 +0300

    Добавьте файлы проекта.

---
+++ GSPITestTasks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GSPITestTasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
