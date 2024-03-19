b99eba941bc8b61eb2bff80f4df34f11dcee0080
Georgeiy <GV.MainG@gmail.com>
2024-03-19 18:00:28 +0300

    Task2

---
+++ GSPITestTasksTest/Task2Test.cs
@@ -0,0 +1,20 @@
+
+
+
+namespace GSPITestTasksTest
+{
+    public class Task2Test
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        public void CountNormalSubstringsTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
