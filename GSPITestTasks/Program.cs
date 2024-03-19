7d6a6d29bb7da00df69b942c3268b2907e5fe663
Georgeiy <GV.MainG@gmail.com>
2024-03-19 18:00:42 +0300

    Task2

--- GSPITestTasks/Program.cs
+++ GSPITestTasks/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace GSPITestTasks
+﻿using GSPITestTasks.Tasks;
+
+namespace GSPITestTasks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string input = "aacaba";
+            int result = Task2.CountNormalSubstrings(input);
+
+            Console.WriteLine($"Исходная строка: {input}");
+            Console.WriteLine($"Количество нормальных разбиений строки: {result}");
         }
     }
 }
